815e0aaca4fe94f8e5120a80c662d46a4aff3725
Lukas <lukas-nagel@wlan244-80.extern.hu-berlin.de>
2014-02-24 13:04:24 +0100

    Can you try this code?

--- SarahGame.py
+++ SarahGame.py
@@ -1,1 +1,16 @@
 #SarahGame
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+running=True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SLASH:
+            running=False
